b8a68d6f0bbbdc051745cb9fd69f6239cfae8ae3
Muneer Pinjar <munna.ieee@gmail.com>
2023-10-06 07:30:38 +0530

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCCrudOperationDemo.MvcApplication" Language="C#" %>
---
+++ Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCrudOperationDemo.Models
+{
+    [Bind(Exclude = "action")]
+    public class Customer
+    {
+        public int action { get; set; }
+
+        public int customer_id { get; set; }
+       
+        [Required(ErrorMessage ="*")]
+        public string customer_name { get; set; }
+       
+        public string address_text { get; set; }
+       
+        public string contact_no { get; set; }
+        [Required(ErrorMessage = "*")]
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public DataTable customerDT { get; set; }
+    }
+}
